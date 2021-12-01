659725d6b9682655a416609c991910caea03a52a
Luminiscental <luminiscental01@gmail.com>
2021-12-01 08:36:50 +0000

    Solve day 1

--- src/day01.rs
+++ src/day01.rs
@@ -1,22 +1,29 @@
 use super::day::Day;
+use itertools::izip;
 
 pub struct Day01 {}
 
 impl Day for Day01 {
-    type Input = String;
-    type ProcessedInput = String;
+    type Input = Vec<i32>;
+    type ProcessedInput = Self::Input;
 
     const DAY: usize = 1;
 
     fn parse(input: String) -> Self::Input {
-        input
+        input.lines().map(|line| line.parse().unwrap()).collect()
     }
 
     fn solve_part1(input: Self::Input) -> (Self::ProcessedInput, String) {
-        (input, String::new())
+        let deltas = izip!(input.iter(), input.iter().skip(1)).map(|(a, b)| b - a);
+        let increases = deltas.filter(|&d| d > 0).count();
+        (input, increases.to_string())
     }
 
-    fn solve_part2(_input: Self::ProcessedInput) -> String {
-        String::new()
+    fn solve_part2(input: Self::ProcessedInput) -> String {
+        let windows = izip!(input.iter(), input.iter().skip(1), input.iter().skip(2));
+        let sums: Vec<_> = windows.map(|(a, b, c)| a + b + c).collect();
+        let deltas = izip!(sums.iter(), sums.iter().skip(1)).map(|(a, b)| b - a);
+        let increases = deltas.filter(|&d| d > 0).count();
+        increases.to_string()
     }
 }
