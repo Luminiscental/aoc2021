e50e453a67657f26baa1c49a0bde7727a39f3d73
Luminiscental <luminiscental01@gmail.com>
2021-12-14 14:14:18 +0000

    Simplify day 14

--- src/day14.rs
+++ src/day14.rs
@@ -1,15 +1,8 @@
 use super::day::Day;
 use itertools::Itertools;
-use std::{collections::HashMap, mem};
+use std::{collections::HashMap, iter, mem};
 
 type Pair = (u8, u8);
-
-pub struct Input {
-    first_elmt: u8,
-    last_elmt: u8,
-    polymer: HashMap<Pair, usize>,
-    rules: HashMap<Pair, u8>,
-}
 
 fn reinforce(polymer: &mut HashMap<Pair, usize>, rules: &HashMap<Pair, u8>) {
     for (pair, count) in mem::take(polymer).into_iter() {
@@ -18,15 +11,11 @@ fn reinforce(polymer: &mut HashMap<Pair, usize>, rules: &HashMap<Pair, u8>) {
     }
 }
 
-fn diversity(first_elmt: u8, last_elmt: u8, polymer: &HashMap<Pair, usize>) -> usize {
+fn diversity(last: u8, polymer: &HashMap<Pair, usize>) -> usize {
     let mut counts = HashMap::new();
-    for (pair, count) in polymer.iter() {
+    for (pair, count) in polymer.iter().chain(iter::once((&(last, 0), &1))) {
         *counts.entry(pair.0).or_insert(0) += count;
-        *counts.entry(pair.1).or_insert(0) += count;
     }
-    *counts.entry(first_elmt).or_insert(0) += 1;
-    *counts.entry(last_elmt).or_insert(0) += 1;
-    counts.values_mut().for_each(|n| *n /= 2);
     let (min, max) = counts.into_values().minmax().into_option().unwrap();
     max - min
 }
@@ -34,33 +23,32 @@ fn diversity(first_elmt: u8, last_elmt: u8, polymer: &HashMap<Pair, usize>) -> u
 pub struct Day14;
 
 impl<'a> Day<'a> for Day14 {
-    type Input = Input;
+    type Input = (u8, HashMap<Pair, usize>, HashMap<Pair, u8>);
     type ProcessedInput = Self::Input;
 
     const DAY: usize = 14;
 
     fn parse(input: &'a str) -> Self::Input {
         let (template, rules) = input.split("\n\n").next_tuple().unwrap();
-        Input {
-            first_elmt: template.bytes().next().unwrap(),
-            last_elmt: template.bytes().last().unwrap(),
-            polymer: template.bytes().tuple_windows().counts(),
-            rules: rules
+        (
+            template.bytes().last().unwrap(),
+            template.bytes().tuple_windows().counts(),
+            rules
                 .lines()
                 .map(|line| line.split(" -> ").next_tuple().unwrap())
                 .map(|(pair, elmt)| ((pair.as_bytes()[0], pair.as_bytes()[1]), elmt.as_bytes()[0]))
                 .collect(),
-        }
+        )
     }
 
-    fn solve_part1(mut input: Self::Input) -> (Self::ProcessedInput, String) {
-        (0..10).for_each(|_| reinforce(&mut input.polymer, &input.rules));
-        let ans = diversity(input.first_elmt, input.last_elmt, &input.polymer);
-        (input, ans.to_string())
+    fn solve_part1((last, mut polymer, rules): Self::Input) -> (Self::ProcessedInput, String) {
+        (0..10).for_each(|_| reinforce(&mut polymer, &rules));
+        let ans = diversity(last, &polymer);
+        ((last, polymer, rules), ans.to_string())
     }
 
-    fn solve_part2(mut input: Self::ProcessedInput) -> String {
-        (0..30).for_each(|_| reinforce(&mut input.polymer, &input.rules));
-        diversity(input.first_elmt, input.last_elmt, &input.polymer).to_string()
+    fn solve_part2((last, mut polymer, rules): Self::ProcessedInput) -> String {
+        (0..30).for_each(|_| reinforce(&mut polymer, &rules));
+        diversity(last, &polymer).to_string()
     }
 }
