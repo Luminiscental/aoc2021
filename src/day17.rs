0915c408792cc616cf2b3a3f44378603e3262ecc
Luminiscental <luminiscental01@gmail.com>
2021-12-17 22:04:17 +0000

    Comment an explanation for day 17

--- src/day17.rs
+++ src/day17.rs
@@ -81,3 +81,57 @@ mod test_day17 {
 }
 
 bench_day!(17);
+
+/*
+ * How observe_preimages works:
+ *
+ *   The problem is essentially asking for the set of velocities (v_x, v_y) such
+ *   that the trajectory enters the target at some step. In both directions, our
+ *   velocity decreases by 1 each step, but in the x direction this stops at 0,
+ *   whereas in the y direction it continues indefinitely.
+ *
+ *   For n=1,2,3,... define f_n(v) = v + (v-1) + (v-2) + ... + (v-n+1) which is
+ *   the distance travelled after n steps starting with velocity v (in the y
+ *   direction at least). Then define a modified function g_n(v) by
+ *   g_n(v) = f_{max(v, n)}(v), which models the stopping that happens in the x
+ *   direction; if we take more than v steps we still get f_v(v).
+ *
+ *   Then (g_n(v_x), f_n(v_y)) gives the point reached after n steps with
+ *   initial velocities (v_x, v_y), so the set of velocities which eventually
+ *   hit the target is the union over n of the preimages of the target region
+ *   under (g_n, f_n).
+ *
+ *   Now there are two possible ways for (g_n(v_x), f_n(v_y)) to equal a given
+ *   point (x, y). If n < v_x, then g_n(v_x) = f_n(v_x), so (x, y) is a preimage
+ *   under the simpler function (f_n, f_n), which is easily dealt with since f_n
+ *   is an invertible linear function (so we can just apply it to the endpoints
+ *   of the target ranges in x and y):
+ *
+ *       f_n(v) = v + (v-1) + (v-2) + ... + (v-n+1)
+ *              = n*v - (1 + 2 + ... + (n-1))
+ *              = n*v - n*(n-1)/2
+ *
+ *       f_n^{-1}(x) = x/n + (n-1)/2
+ *
+ *   Otherwise, n >= v_x so g_n(v_x) = g_{v_x}(v_x) is a triangular number (if
+ *   k = v_x, then the kth triangular number t(k) = k + (k-1) + (k-2) + ...
+ *   + 2 + 1 = g_k(k)). Hence the other preimages will correspond to triangular
+ *   numbers within the x range of our target region (in particular there are
+ *   not likely to be many of these edge case x values). So fix some velocity
+ *   v_x corresponding to one of the triangular numbers g_{v_k}(v_k) inside the
+ *   target x range. We simply want to find the velocities v_y such that
+ *   f_m(v_y) is in the target y range for some m >= v_k, since in that case
+ *   g_m(v_k) = g_{v_k}(v_k) is in the target x range by assumption. For
+ *   f_m(v_y) to lie in the target range, we need v_y to be at least
+ *   f_m^{-1}(target_y_min) since f_m(x) is an increasing function of x, and
+ *   hence at least f_{v_k}^{-1}(target_y_min) since f_m(x) is an increasing
+ *   function of m also, and we're assuming m >= v_k. This gives a lower bound
+ *   on the values of v_y that need checking. To get the upper bound, notice
+ *   that if v_y is larger than the furthest extent of the target region from
+ *   y=0, then we are guaranteed to miss the entire target when we fall down,
+ *   since we reach y=0 in correspondence with the initial state, and then take
+ *   a step of -v_y. Hence we can iterate through this range of values of v_y,
+ *   and for each value check whether it hits the target by solving the
+ *   quadratic equations f_m(v_y) = target_y_min and f_m(v_y) = target_y_max for
+ *   m, and checking whether an integer value of m lies inbetween those roots.
+ */
